76fe0d680aa34fd879d036eadea707070e9fa8a6
Gary Zhang <0619cloudz@gmail.com>
2023-05-30 16:59:18 -0400

    update v0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,71 @@
-import { greetUser } from '$utils/greet';
+import { showVersion } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar-main-wrap'); // Replace "navbar" with the ID of your navbar element
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const navbarDropdownMenu = document.querySelectorAll('.nav-dropdown-wrap');
+
+    if (scrollTop > 0) {
+      navbar.style.height = '80px';
+      navbarDropdownMenu.forEach((menu) => {
+        menu.style.top = '115px'; // Adjust the top position to your desired value
+      });
+    } else {
+      navbar.style.height = '130px';
+      navbarDropdownMenu.forEach((menu) => {
+        menu.style.top = '165px'; // Adjust the top position to your desired value
+      });
+    }
+  });
+  // Get All Videos
+  const servicesCards = document.querySelectorAll('.services-card');
+
+  //Loop over videos to add eventListeners
+  for (const card of servicesCards) {
+    const video = card.querySelector('.service-video-bg video');
+    //Play on Mouseover
+    card.addEventListener(
+      'mouseover',
+      function () {
+        video.play();
+      },
+      false
+    );
+
+    card.addEventListener(
+      'mouseout',
+      function () {
+        video.pause();
+      },
+      false
+    );
+  }
+
+  // Get All Videos
+  const otherServiceCards = document.querySelectorAll('.other-services-card');
+
+  //Loop over videos to add eventListeners
+  for (const otherServiceCard of otherServiceCards) {
+    const video = otherServiceCard.querySelector('.service-video-bg video');
+    //Play on Mouseover
+    otherServiceCard.addEventListener(
+      'mouseover',
+      function () {
+        video.play();
+      },
+      false
+    );
+
+    otherServiceCard.addEventListener(
+      'mouseout',
+      function () {
+        video.pause();
+      },
+      false
+    );
+  }
+  const version = 'v0.0.1';
+  showVersion(version);
 });
--- src/utils/greet.ts
+++ src/utils/greet.ts
@@ -1,19 +1,9 @@
 /* eslint-disable no-console */
-import { getPublishDate } from '@finsweet/ts-utils';
 
 /**
  * Greets the user by printing a message in the console.
  * @param name The user's name.
  */
-export const greetUser = (name: string) => {
-  const publishDate = getPublishDate();
-
-  console.log(`Hello ${name}!`);
-  console.log(
-    `This site was last published on ${publishDate?.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: '2-digit',
-    })}.`
-  );
+export const showVersion = (version: string) => {
+  console.log(`Version: ${version}!`);
 };
